e8a23d3e406caee00355eef25f7b1fbd1c10b44e
kintan <kingslay@icloud.com>
2023-09-26 22:28:59 +0800

    格式化

--- Package.swift
+++ Package.swift
@@ -4,8 +4,9 @@ import PackageDescription
 let package = Package(
     name: "FFmpegKit",
     defaultLocalization: "en",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13),
-    ],
+    platforms: [.macOS(.v10_15),
+                .iOS(.v13),
+                .tvOS(.v13)],
     products: [
         .library(
             name: "FFmpegKit",
